baf6268a9e2edb80d99519b42bc8786279a16a8e
RayanSp <rayan.spall@hva.nl>
2023-01-30 12:57:55 +0100

    javascript interactie

---
+++ script.js
@@ -0,0 +1,85 @@
+function expandstep1() {
+    var contentstep = document.getElementById("content-step-1");
+    if (contentstep.style.display === "none") {
+        contentstep.style.display = "block";
+        window.setTimeout(function(){
+            contentstep.style.opacity = 1;
+            contentstep.style.transform = 'scale(1)';
+        },0);
+} else {
+    contentstep.style.transform = "scale(0)";
+    contentstep.style.opacity = 0;
+    window.setTimeout(function(){
+        contentstep.style.display = "none";
+    },350);
+    }
+}
+
+function expandstep2() {
+    var contentstep = document.getElementById("content-step-2");
+    if (contentstep.style.display === "none") {
+        contentstep.style.display = "block";
+        window.setTimeout(function(){
+            contentstep.style.opacity = 1;
+            contentstep.style.transform = 'scale(1)';
+        },0);
+} else {
+    contentstep.style.transform = "scale(0)";
+    contentstep.style.opacity = 0;
+    window.setTimeout(function(){
+        contentstep.style.display = "none";
+    },350);
+    }
+}
+
+function expandstep3() {
+    var contentstep = document.getElementById("content-step-3");
+    if (contentstep.style.display === "none") {
+        contentstep.style.display = "block";
+        window.setTimeout(function(){
+            contentstep.style.opacity = 1;
+            contentstep.style.transform = 'scale(1)';
+        },0);
+} else {
+    contentstep.style.transform = "scale(0)";
+    contentstep.style.opacity = 0;
+    window.setTimeout(function(){
+        contentstep.style.display = "none";
+    },350);
+    }
+}
+
+function expandstep4() {
+    var contentstep = document.getElementById("content-step-4");
+    if (contentstep.style.display === "none") {
+        contentstep.style.display = "block";
+        window.setTimeout(function(){
+            contentstep.style.opacity = 1;
+            contentstep.style.transform = 'scale(1)';
+        },0);
+} else {
+    contentstep.style.transform = "scale(0)";
+    contentstep.style.opacity = 0;
+    window.setTimeout(function(){
+        contentstep.style.display = "none";
+    },350);
+    }
+}
+
+function expandstep5() {
+    var contentstep = document.getElementById("content-step-5");
+    if (contentstep.style.display === "none") {
+        contentstep.style.display = "block";
+        window.setTimeout(function(){
+            contentstep.style.opacity = 1;
+            contentstep.style.transform = 'scale(1)';
+        },0);
+} else {
+    contentstep.style.transform = "scale(0)";
+    contentstep.style.opacity = 0;
+    window.setTimeout(function(){
+        contentstep.style.display = "none";
+    },350);
+    }
+}
+
